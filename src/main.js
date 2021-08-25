779c48f26974eb07c5bfe8f05c8abdd8e4ae3e44
ShieldFor <37861219+shieldFor@users.noreply.github.com>
2021-08-26 01:21:57 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,4 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.config.productionTip = false
-Vue.use(Element)
-console.log('666')
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+(function(){
+   console.log('hello world')
+  console.log('666')
+})()
